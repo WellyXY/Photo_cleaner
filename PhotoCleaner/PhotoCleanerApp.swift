a77db6957fb1830d7e7edb2000f53e6122f5cad4
WellyXY <welly.xy@gmail.com>
2025-04-21 01:39:48 -0700

    Initial Commit

---
+++ PhotoCleaner/PhotoCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCleanerApp.swift
+//  PhotoCleaner
+//
+//  Created by Welly_luo on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoCleanerTests/PhotoCleanerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCleanerTests.swift
+//  PhotoCleanerTests
+//
+//  Created by Welly_luo on 4/21/25.
+//
+
+import Testing
+@testable import PhotoCleaner
+
+struct PhotoCleanerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
